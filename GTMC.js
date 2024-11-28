d125274ad6791fe82387e33c4bfdbc614042e097
Pual397 <65188426+Pual397@users.noreply.github.com>
2024-11-28 23:02:36 +0800

    Update GTMC.js

--- GTMC.js
+++ GTMC.js
@@ -6,4 +6,8 @@
 ^https:\/\/carapp\.gtmc\.com\.cn\/bff\/app\/servicePage\/service\/getServiceDataByType url reject-dict
 #数字展馆
 ^https:\/\/carapp\.gtmc\.com\.cn\/bff\/app\/servicePage\/service\/getBrandPavilion url reject-dict
+#禁用更新检测
+^https:\/\/carapp\.gtmc\.com\.cn\/api\/appgtmc\/appVersion\/AppVersionManager\/checkNewAppVersion\.json url reject-dict
+#不知道是什么
+^https:\/\/carapp\.gtmc\.com\.cn\/\/bff\/app\/servicePage\/service\/queryAgreementData url reject-dict
 hostname = carapp.gtmc.com.cn
