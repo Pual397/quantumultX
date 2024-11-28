0e72d4bb63c51b653a99fb53c61aecc482882f31
Pual397 <65188426+Pual397@users.noreply.github.com>
2024-11-28 22:38:35 +0800

    Create GTMC.js
    丰云行APP去广告

---
+++ GTMC.js
@@ -0,0 +1,9 @@
+#右侧浮动广告
+^https:\/\/carapp\.gtmc\.com\.cn\/bff\/app\/advert\/cmsManage\/queryAdvertList url reject-dict
+#XX列表
+^https:\/\/carapp\.gtmc\.com\.cn\/bff\/app\/servicePage\/service\/getEnjoyList url reject-dict
+#保养预约、悦享加油、ETC、故障灯查询
+^https:\/\/carapp\.gtmc\.com\.cn\/bff\/app\/servicePage\/service\/getServiceDataByType url reject-dict
+#数字展馆
+^https:\/\/carapp\.gtmc\.com\.cn\/bff\/app\/servicePage\/service\/getBrandPavilion url reject-dict
+hostname = carapp.gtmc.com.cn
